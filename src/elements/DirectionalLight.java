e32fdc9a6ef3d51e5cecfe4bdc056473abedd489
Acher1234 <acherklein0@gmail.com>
2020-06-05 09:57:36 +0300

    new version

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -8,20 +8,39 @@ public class DirectionalLight extends Light implements LightSource {
 
     private Vector _direction;
 
+    /**
+     * Instantiates a new DirectionalLight.
+     * @param _intensity
+     * @param direction
+     */
     public DirectionalLight(Color _intensity,Vector  direction) {
         super(_intensity);
         _direction = direction;
     }
 
+    /**
+     * gets L
+     * @return
+     */
     public Vector getL() {
         return _direction.normalized();
     }
 
+    /**
+     * gets intensity.
+     * @param p
+     * @return
+     */
     @Override
     public Color getIntensity(Point3D p) {
         return _intensity;
     }
 
+    /**
+     * gets L.
+     * @param p
+     * @return
+     */
     @Override
     public Vector getL(Point3D p) {
         return _direction;
