8633b981f91ad1ee9d0bd7d51129da8903dae8ab
samuel illouz <samuelillouz17@gmail.com>
2020-06-25 15:59:29 +0300

    test pour scene

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import elements.AmbientLight;
+import elements.Camera;
+import elements.reelSpotLight;
 import geometries.Cylinder;
 import geometries.Sphere;
+import geometries.Triangle;
 import primitives.*;
 import static java.lang.System.out;
 import static primitives.Util.*;
+
+import renderer.ImageWriter;
+import renderer.Render;
+import scene.Scene;
 import unittests.*;
 
 
@@ -20,20 +28,25 @@ public final class Main {
     public static void main(String[] args)
     {
         try{
-            /*
-            new CylinderTest().getNormal();
-            new PolygonTests().testGetNormal();
-            new SphereTest().getNormal();
-            new TubeTest().getNormal();
-            new TriangleTest().getNormal();
-            VectorTests test = new VectorTests();
-            test.crossProduct();
-            test.dotProduct();
-            test.lenghtSquared();
-            test.normalized();
-            test.scale();
+            Scene scene = new Scene("Test scene");
+            scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+            scene.setDistance(1000);
+            scene.setBackground(Color.BLACK);
+            scene.setAmbientLight(new AmbientLight(Color.BLACK, 0));
 
-             */
+            scene.addGeometries(new Triangle(new Color(java.awt.Color.BLUE), new Material(0.5, 0.5, 30), //
+                    new Point3D(-70, 40, 0), new Point3D(-40, 70, 0), new Point3D(-68, 68, 4)),
+                    new Triangle(new Color(java.awt.Color.BLUE), new Material(0.5, 0.5, 30), //
+                            new Point3D(-70, 40, 0), new Point3D(-40, 70, 0), new Point3D(-68, 68, 4)));
+
+            scene.addLights(new reelSpotLight(new Color(400, 240, 0), //
+                    new Point3D(-100, 100, -200), 1, 1E-5, 1.5E-7,5));
+
+            ImageWriter imageWriter = new ImageWriter("SceneFinal", 1000, 700, 2000, 1400);
+            Render render = new Render(imageWriter, scene);
+
+            render.renderImage();
+            render.writeToImage();
         }
         catch (Exception e)
         {
