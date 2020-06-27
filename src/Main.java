fd1ba417400faf308a68186c9f25afe72cb1b580
samuel illouz <samuelillouz17@gmail.com>
2020-06-28 02:00:26 +0300

    LES RIDEAUX ENFIIIINNNNN

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,30 @@ public final class Main {
     {
         Scene scene = new Scene("Scene final");
         scene.setCamera(new Camera(new Point3D(500, 1200, 100), new Vector(0, -1, 0), new Vector(0, 0, 1)));
-        scene.setDistance(900);
+        scene.setDistance(180);
         scene.setBackground(new Color(222,184,135));
         scene.setAmbientLight(new AmbientLight(Color.BLACK, 0));
 
         scene.addGeometries(
+
+                //2 spheres pour le haut de la scene en mode rideau
+                //sphere droite
+                new Sphere(new Color(255,0,0), new Material(0.5, 0.5, 30), //
+                        360, new Point3D(-250, 0, 450)),
+                //sphere gauche
+                new Sphere(new Color(255,0,0), new Material(0.5, 0.5, 30), //
+                        360, new Point3D(1250, 0, 450)),
+
+                //2 triangle pour faire le rectangle aus dessus de la scene
+                //triangle droite
+                new Triangle(new Color(255,0,0), new Material(0.5, 0.5, 30),
+                        new Point3D(-100, 150, 550), new Point3D(1400, 150, 550), new Point3D(-100, 150, 400)),
+                //triangle gauche
+                new Triangle(new Color(255,0,0), new Material(0.5, 0.5, 30),
+                        new Point3D(1400, 150, 550), new Point3D(-100, 150, 400), new Point3D(1400, 150, 400)),
+
+
+
                 //plancher de la scene
                 new Triangle(new Color(40,20,0), new Material(1, 0.5, 50),
                         new Point3D(-100, 0, 10), new Point3D(1100, 0, 10), new Point3D(1100, 300, -22)),
@@ -52,7 +71,12 @@ public final class Main {
                 new Cylinder(new Color(100,100,100),new Material(0.5,1,30,0.5,0),20,new Ray(new Point3D(280,60,30),new Vector(1,0,0)),20),
                 //Lumiere
                 new Sphere(new Color(255,255,255),new Material(0.5,0.5,30,0,0.2),80,new Point3D(500,0,400))
+
+
         );
+
+        scene.addLights(new SpotLight(new Color(400, 240, 0), //
+                new Point3D(-100, 100, -200), new Vector(1, -1, 3), 1, 1E-5, 1.5E-7));
 
         scene.addLights(
                 new reelSpotLight(new Color(255, 255, 255), //
@@ -61,7 +85,7 @@ public final class Main {
                         new Point3D(200,100,500), new Vector(0,0,-1), 1, 0.00001,0.000000001,60)
         );
 
-                ImageWriter imageWriter = new ImageWriter("SceneFinal", 1000, 700, 2000, 1400);
+                ImageWriter imageWriter = new ImageWriter("SceneFinal", 200, 140, 400, 280);
         Render render = new Render(imageWriter, scene);
 
         render.renderImage();
--- src/unittests/ShadowTests.java
+++ src/unittests/ShadowTests.java
@@ -55,6 +55,7 @@ public class ShadowTests {
 				new Triangle(new Color(java.awt.Color.BLUE), new Material(0.5, 0.5, 30), //
 						new Point3D(-70, 40, 0), new Point3D(-40, 70, 0), new Point3D(-68, 68, 4)));
 
+		//ca cest la lumiere de shadow stam pour voir avec une variation
 		scene.addLights(new SpotLight(new Color(400, 240, 0), //
 				new Point3D(-100, 100, -200), new Vector(1, -1, 3), 1, 1E-5, 1.5E-7));
 
