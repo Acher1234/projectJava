acc7303c6ea886c22e60d14e701eb8f648246530
Acher1234 <acherklein0@gmail.com>
2020-07-14 10:52:13 +0300

    version final

---
+++ src/unittests/SphereTest.java
@@ -0,0 +1,94 @@
+package unittests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import geometries.*;
+import primitives.*;
+
+import java.util.List;
+
+/**
+ * The type Sphere test.
+ */
+public class SphereTest {
+
+    /**
+     * Gets normal.
+     */
+    @Test
+    public void getNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        Point3D centerTest = new Point3D(1,0,1);
+        Sphere Stest = new Sphere(1.0,new Point3D(0,0,0));
+        Vector result = Stest.getNormal(centerTest);
+        Vector test = new Vector(-1,0,-1).normalized();
+        assertEquals(result.getHead().getCoordX(),test.getHead().getCoordX());
+        assertEquals(result.getHead().getCoordY(),test.getHead().getCoordY());
+        assertEquals(result.getHead().getCoordZ(),test.getHead().getCoordZ());
+    }
+
+    /**
+     * Find intersection.
+     */
+    @Test
+    public void findIntersection() {
+        Sphere test = new Sphere(1.0, new Point3D(0, 0, 0));
+        //EP TEST 1--------
+        //1 test for EP
+        List<Intersectable.GeoPoint> result = test.findIntersection(new Ray(new Vector(1.86, 2.44, -0.24), new Point3D(-0.34, -0.65, 0.68)));
+        Point3D resultPoint = result.get(0).point;
+        assertEquals(resultPoint, new Point3D(-0.34, -0.65, 0.68));
+        resultPoint = result.get(1).point;
+        assertEquals(resultPoint, new Point3D(0.60, 0.58, 0.56));
+        //2 test for EP
+        result = test.findIntersection(new Ray(new Vector(1, 0, 0), new Point3D(1, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(resultPoint, new Point3D(1.0, 0, 0));
+        //EP test 2
+        //1 test for EP
+        result = test.findIntersection(new Ray(new Vector(-3, 0, 0), new Point3D(2, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(1, 0, 0), resultPoint);
+        resultPoint = result.get(1).point;
+        assertEquals(new Point3D(-1, 0, 0), resultPoint);
+        //2 test for EP
+        result = test.findIntersection(new Ray(new Vector(-3, 0, 0), new Point3D(0.5, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(-1, 0, 0), resultPoint);
+        //3 test for EP
+        result = test.findIntersection(new Ray(new Vector(-1, 0, 0), new Point3D(-1.5, 0, 0)));
+        assertNull(result);
+        //4 test for EP
+        result = test.findIntersection(new Ray(new Vector(-2.1, 0, 0), new Point3D(1, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(1, 0, 0), resultPoint);
+        resultPoint = result.get(1).point;
+        assertEquals(new Point3D(-1, 0, 0), resultPoint);
+        //5 test for EP
+        result = test.findIntersection(new Ray(new Vector(0.47, 0.27, 0.84), new Point3D(0, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(0.47, 0.27, 0.84), resultPoint);
+        //6 test for EP
+        result = test.findIntersection(new Ray(new Vector(1, 0, 0), new Point3D(1, 0, 0)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(1, 0, 0), resultPoint);
+        //EP TEST 3
+        //test 1
+        result = test.findIntersection(new Ray((new Vector(-0.65, -0.39, 0.95)), new Point3D(0, 0, 1.61)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(-0.65, -0.39, 0.65), resultPoint);
+        //test 2
+        result = test.findIntersection(new Ray((new Vector(0.65, 0.39, -0.95)).scale(2), new Point3D(-0.65, -0.39, 0.65)));
+        resultPoint = result.get(0).point;
+        assertEquals(new Point3D(-0.65, -0.39, 0.65), resultPoint);
+        //test3
+        result = test.findIntersection(new Ray((new Vector(1, 1, 1)), new Point3D(2, 2, 2)));
+        assertNull(result);
+        //BVA test
+        result = test.findIntersection(new Ray((new Vector(0, 0, 2)), new Point3D(2, 0, 0)));
+        assertNull(result);
+
+    }
+
+}
---
+++ src/unittests/cameraRayIntegrationTest.java
@@ -0,0 +1,218 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import elements.Camera;
+import primitives.*;
+import geometries.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class cameraRayIntegrationTest {
+
+    @Test
+    public void TestsphereFunction()
+    {
+        Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        ArrayList<Ray> rayTest= new ArrayList<Ray>();
+        ArrayList<Point3D> pointTest= new ArrayList<Point3D>();
+        List<Intersectable.GeoPoint> pointTemp ;
+        for (int i = 0;i < 3;i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                rayTest.add(camera.constructRayThroughPixel(3,3,j,i,1,3,3));
+            }
+        }
+        //Sphere Test
+        Sphere sphereTest = new Sphere(1,new Point3D(0,0,3));
+        for (Ray temp:rayTest)
+        {
+            pointTemp = sphereTest.findIntersection(temp);
+            if(pointTemp == null)
+            {
+                continue;
+            }
+            for (Intersectable.GeoPoint p3D:pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(2,pointTest.size(),0);
+        pointTest.clear();
+
+        //sphere test4
+        sphereTest = new Sphere(4,new Point3D(0,0,0));
+        for (Ray temp:rayTest)
+        {
+            pointTemp = sphereTest.findIntersection(temp);
+            if(pointTemp == null)
+            {
+                continue;
+            }
+            for (Intersectable.GeoPoint p3D:pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(9,pointTest.size(),0);
+        pointTest.clear();
+        //sphere test5
+        sphereTest = new Sphere(0.5,new Point3D(0,0,-1));
+        for (Ray temp:rayTest)
+        {
+            pointTemp = sphereTest.findIntersection(temp);
+            if(pointTemp == null)
+            {
+                continue;
+            }
+            for (Intersectable.GeoPoint p3D:pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(0,pointTest.size(),0);
+        pointTest.clear();
+        //Sphere test2
+        camera = new Camera(new Point3D(0,0,-0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        rayTest.clear();
+        for (int i = 0;i < 3;i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                rayTest.add(camera.constructRayThroughPixel(3,3,j,i,1,3,3));
+            }
+        }
+        sphereTest = new Sphere(2.5,new Point3D(0,0,2.5));
+        for (Ray temp:rayTest)
+        {
+            pointTemp = sphereTest.findIntersection(temp);
+            if(pointTemp == null)
+            {
+                continue;
+            }
+            for (Intersectable.GeoPoint p3D:pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(18,pointTest.size(),0);
+        pointTest.clear();
+        //sphere Test 3
+        sphereTest = new Sphere(2,new Point3D(0,0,2));
+        for (Ray temp:rayTest)
+        {
+            pointTemp = sphereTest.findIntersection(temp);
+            if(pointTemp == null)
+            {
+                continue;
+            }
+            for (Intersectable.GeoPoint p3D:pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(10,pointTest.size(),0);
+        pointTest.clear();
+    }
+    @Test
+    public void TestplaneFunction()
+    {
+        Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        ArrayList<Ray> rayTest= new ArrayList<Ray>();
+        ArrayList<Point3D> pointTest= new ArrayList<Point3D>();
+        List<Intersectable.GeoPoint> pointTemp ;
+        for (int i = 0;i < 3;i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                rayTest.add(camera.constructRayThroughPixel(3,3,j,i,1,3,3));
+            }
+        }
+        Plane planetest = new Plane(new Point3D(0,0,7), camera.getVto());
+        for (Ray temp: rayTest)
+        {
+            pointTemp = planetest.findIntersection(temp);
+            if(pointTemp == null) continue;
+            for(Intersectable.GeoPoint p3D: pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(9,pointTest.size(),0);
+        pointTest.clear();
+        //test plane 2
+        planetest = new Plane(new Point3D(0,0,7),new Vector(0,-1,-2));
+        for (Ray temp: rayTest)
+        {
+            pointTemp = planetest.findIntersection(temp);
+            if(pointTemp == null) continue;
+            for(Intersectable.GeoPoint p3D: pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(9,pointTest.size(),0);
+        pointTest.clear();
+        //test plane 3
+        planetest = new Plane(new Point3D(0,0,7),new Vector(1,0,1));
+        for (Ray temp: rayTest)
+        {
+            pointTemp = planetest.findIntersection(temp);
+            if(pointTemp == null) continue;
+            for(Intersectable.GeoPoint p3D: pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(6,pointTest.size(),0);
+        pointTest.clear();
+
+    }
+    @Test
+    public void TriangleTest()
+    {
+        Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        ArrayList<Ray> rayTest= new ArrayList<Ray>();
+        ArrayList<Point3D> pointTest= new ArrayList<Point3D>();
+        List<Intersectable.GeoPoint> pointTemp ;
+        for (int i = 0;i < 3;i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                rayTest.add(camera.constructRayThroughPixel(3,3,j,i,1,3,3));
+            }
+        }
+        //triangle test 1
+        Triangle triangleTest = new Triangle(new Point3D(0,-1,2),new Point3D(1,1,2),new Point3D(-1,1,2));
+        for (Ray temp: rayTest)
+        {
+            pointTemp = triangleTest.findIntersection(temp);
+            if(pointTemp == null) continue;
+            for(Intersectable.GeoPoint p3D: pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(1,pointTest.size(),0);
+        pointTest.clear();
+        //triangle test 2
+        triangleTest = new Triangle(new Point3D(0,-20,2),new Point3D(1,1,2),new Point3D(-1,1,2));
+        for (Ray temp: rayTest)
+        {
+            pointTemp = triangleTest.findIntersection(temp);
+            if(pointTemp == null) continue;
+            for(Intersectable.GeoPoint p3D: pointTemp)
+            {
+                pointTest.add(p3D.point);
+            }
+        }
+        assertEquals(2,pointTest.size(),0);
+        pointTest.clear();
+
+    }
+
+
+}
